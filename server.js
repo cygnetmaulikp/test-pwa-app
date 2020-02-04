5afc0ed0deaabfba98ddac4162ab0b84d93c36d1
maulikprajapati <maulikprajapati47@gmail.com>
2020-02-04 23:06:20 +0530

    changes

--- server.js
+++ server.js
@@ -1,33 +1,15 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
-const router = express.Router();
 
-const pathToBuild = '../pwa-test-app/dist/pwa-test-app';
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/pwa-test-app'));
 
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/pwa-test-app/index.html'));
+});
 
-app.use(express.static(pathToBuild, { etag: false }));
-
-router.get('/countries', (req, res) => {
-    const list = ['india', 'australia', 'us'];
-    res.status(200).json(list);
-})
-// app.use(router);
-
-app.use('/api', router);
-
-// fallback
-// app.get("*", function (req, res) {
-//     const file = path.resolve(pathToBuild, "index.html");
-//     res.sendFile(file);
-// });
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, (err) => {
-    if (err) return console.error(err);
-    console.log(`app is listening on port ${PORT}`);
-
-})
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
